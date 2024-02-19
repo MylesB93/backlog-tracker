161f306a99e06f87bb9df2c1ba2b33d0612cb806
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-02-19 18:43:46 +0000

    Update database and scaffold identity

--- BacklogTracker/obj/Debug/net8.0/BacklogTracker.AssemblyInfo.cs
+++ BacklogTracker/obj/Debug/net8.0/BacklogTracker.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BacklogTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de04269750471beb225342ecc2b9328588f02159")]
 [assembly: System.Reflection.AssemblyProductAttribute("BacklogTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BacklogTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
