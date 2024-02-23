3f5ad5f79e8bb5f3cc0da0d66df34e6d29f96878
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-02-23 22:27:28 +0000

    Add api request on search

---
+++ BacklogTracker/Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace BacklogTracker.Models
+{
+    public class Game
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("api_detail_url")]
+        public string ApiDetailUrl { get; set; }
+
+        [XmlElement("resource_type")]
+        public string ResourceType { get; set; } // TODO: Change this to enum?
+    }
+}
--- BacklogTracker/Pages/Index.cshtml.cs
+++ BacklogTracker/Pages/Index.cshtml.cs
@@ -1,5 +1,13 @@
+using BacklogTracker.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Policy;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using BacklogTracker.Data;
 
 namespace BacklogTracker.Pages
 {
@@ -17,7 +25,42 @@ namespace BacklogTracker.Pages
 
         }
 
+        public void OnPost()
+        {
+            var query = Request.Form["query"];
+
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri("https://www.giantbomb.com/api/search/");
+            client.DefaultRequestHeaders.Add("User-Agent", "My Awesome App");
+            // Get data response
+            var response = client.GetAsync($"?api_key=971b75abd389a994ecfcc70ae5ebab3e2abe2a35&query={query}&resources=game&field_list=name,api_detail_url").Result;
+
+            XmlSerializer xs = new XmlSerializer(typeof(Response));
+
+            using (StreamReader reader = new StreamReader(response.Content.ReadAsStreamAsync().Result))
+            {
+                var results = (Response)xs.Deserialize(reader);
+                _logger.LogInformation("Deserialization complete!");
+            }
+
+            _logger.LogInformation("Request complete!");
+        }
+
         [BindProperty(SupportsGet = true)]
         public string SearchTerm { get; set; }
+
+
+    }
+    public class Serializer
+    {
+        public T Deserialize<T>(string input) where T : class
+        {
+            System.Xml.Serialization.XmlSerializer ser = new System.Xml.Serialization.XmlSerializer(typeof(T));
+
+            using (StringReader sr = new StringReader(input))
+            {
+                return (T)ser.Deserialize(sr);
+            }
+        }
     }
 }
