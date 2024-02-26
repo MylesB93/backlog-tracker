fe16b1183d02e0ad5a77376f740620507975bd83
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-02-26 22:02:14 +0000

    Change IdentityUser BacklogTrackerUser

--- BacklogTracker/Program.cs
+++ BacklogTracker/Program.cs
@@ -16,7 +16,7 @@ namespace BacklogTracker
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<BacklogTrackerUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddRazorPages();
 
