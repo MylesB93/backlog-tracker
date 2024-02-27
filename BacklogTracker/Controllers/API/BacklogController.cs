3f9570f950f4d0f9b72ced9c68fb1837f8195d81
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-02-27 22:07:37 +0000

    Create API controller (WIP)

---
+++ BacklogTracker/Controllers/API/BacklogController.cs
@@ -0,0 +1,29 @@
+﻿using BacklogTracker.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BacklogTracker.Controllers.API
+{
+    public class BacklogController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public BacklogController(ApplicationDbContext dbContext)
+        { 
+            _dbContext = dbContext; 
+        }
+
+        [HttpPost]
+        public IActionResult PostData([FromBody] string gameIDs, [FromBody] string email)
+        {
+            if (string.IsNullOrEmpty(gameIDs) || string.IsNullOrEmpty(email))
+            {
+                return BadRequest();
+            }
+
+            // WIP
+            //_dbContext.Users.Where(u => u.Email == email)
+
+            return Ok("Data saved successfully.");
+        }
+    }
+}
