217f1a080f98d11a427c64a69cb08f1818644d4a
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-04-05 15:59:29 +0100

    Add jquery function for adding to backlog

--- BacklogTracker/wwwroot/js/site.js
+++ BacklogTracker/wwwroot/js/site.js
@@ -1,1 +1,31 @@
-﻿
+﻿function addToBacklog(element) {
+    //console.log(element.getAttribute("data-game-guid"));
+
+    const apiUrl = '/api/backlog/patch-user-backlog';
+
+    // TODO: Define the data you want to send to the API endpoint
+    const requestData = {
+        data: '{Email:"mylesbroomes@hotmail.co.uk", GameID: "321"}'
+    };
+
+    fetch(apiUrl, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Data successfully sent to the API:', data);
+        })
+        .catch(error => {
+            console.error('There was a problem sending data to the API:', error);
+        });
+
+}
