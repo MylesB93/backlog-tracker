31a3a59e362a28dba063cfbe4d82f01e06f06d09
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-04-15 16:51:09 +0100

    Add GetUsersGamesAsync method to service

--- BacklogTracker/Interfaces/IGameService.cs
+++ BacklogTracker/Interfaces/IGameService.cs
@@ -1,9 +1,11 @@
 ﻿using BacklogTracker.Models;
+using BacklogTracker.Models.UserBacklog;
 
 namespace BacklogTracker.Interfaces
 {
 	public interface IGameService
 	{
 		Task<Response> GetGamesAsync(string? query);
+		Task<List<BacklogGame>> GetUsersGamesAsync(List<string> gameIds);
 	}
 }
--- BacklogTracker/Services/GiantBombService.cs
+++ BacklogTracker/Services/GiantBombService.cs
@@ -3,6 +3,7 @@ using BacklogTracker.Models;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 using System.Xml.Serialization;
 using Microsoft.Extensions.Options;
+using BacklogTracker.Models.UserBacklog;
 
 namespace BacklogTracker.Services
 {
@@ -24,12 +25,12 @@ namespace BacklogTracker.Services
 			using var client = new HttpClient();
 			client.BaseAddress = new Uri("https://www.giantbomb.com/api/search/");
 			client.DefaultRequestHeaders.Add("User-Agent", "Backlog Tracker app");
-			// Get data response
+
 			var response = client.GetAsync($"?api_key={_giantBombConfiguration.Value.GiantBombAPIKey}&query={query}&resources=game&field_list=name,site_detail_url,description,guid").Result;
 
 			XmlSerializer xs = new XmlSerializer(typeof(Response));
 
-			using (StreamReader reader = new StreamReader(response.Content.ReadAsStreamAsync().Result))
+			using (StreamReader reader = new StreamReader(await response.Content.ReadAsStreamAsync()))
 			{
 				try
 				{
@@ -45,6 +46,40 @@ namespace BacklogTracker.Services
 			_logger.LogInformation("Request complete!");
 
 			return gamesResponse;
+		}
+
+		public async Task<List<BacklogGame>> GetUsersGamesAsync(List<string> gameIds)
+		{
+			var gamesList = new List<BacklogGame>();
+
+			using var client = new HttpClient();
+
+			foreach (var gameId in gameIds)
+			{
+				client.BaseAddress = new Uri($"https://www.giantbomb.com/api/game/{gameId}");
+				client.DefaultRequestHeaders.Add("User-Agent", "Backlog Tracker app");
+
+				var response = await client.GetAsync($"?api_key={_giantBombConfiguration.Value.GiantBombAPIKey}");
+
+				XmlSerializer xs = new XmlSerializer(typeof(BacklogGame));
+
+				using (StreamReader reader = new StreamReader(await response.Content.ReadAsStreamAsync()))
+				{
+					try
+					{
+						gamesList.Add((BacklogGame)xs.Deserialize(reader));
+						_logger.LogInformation("Deserialization complete!");
+					}
+					catch (Exception ex)
+					{
+						_logger.LogError("Error occurred during deserialization: {0}", ex.Message);
+					}
+				}
+
+				_logger.LogInformation("Request complete!");
+			}
+
+			return gamesList;
 		}
 	}
 }
