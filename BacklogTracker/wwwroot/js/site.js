56d00a658b313654f0fcb3fe2d56ad34e956d5e1
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-04-22 20:45:07 +0100

    Add ability to remove games from backlog

--- BacklogTracker/wwwroot/js/site.js
+++ BacklogTracker/wwwroot/js/site.js
@@ -34,3 +34,36 @@
         });        
 
 }
+
+function removeFromBacklog(element, email) {
+    const apiUrl = '/api/backlog/remove-game-from-backlog';
+
+    const requestData = {
+        Email: email,
+        GameID: element.getAttribute("data-game-id")
+    };
+
+    fetch(apiUrl, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+    })
+        .then(response => {
+            if (response.ok)
+                return response.json();
+            else
+                throw new Error('Network response was not ok.')
+        })
+        .then(data => {
+            console.log('Data successfully sent to the API:', data);
+            location.reload();
+        })
+        .catch(error => {
+            var errorMsg = document.getElementById("errorMessage");
+            console.error('There was a problem sending data to the API:', error);
+            errorMsg.innerHTML = error;
+            errorMsg.style.visibility = 'visible';
+        });
+}
