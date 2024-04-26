c0a4bb1eb42b17f8a67096263c1cf9dbc4e3bf81
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-04-26 21:16:33 +0100

    Remove unused serializer class

--- BacklogTracker/Pages/Index.cshtml.cs
+++ BacklogTracker/Pages/Index.cshtml.cs
@@ -45,16 +45,4 @@ namespace BacklogTracker.Pages
 			}
 		}
     }
-    public class Serializer
-    {
-        public T Deserialize<T>(string input) where T : class
-        {
-            System.Xml.Serialization.XmlSerializer ser = new System.Xml.Serialization.XmlSerializer(typeof(T));
-
-            using (StringReader sr = new StringReader(input))
-            {
-                return (T)ser.Deserialize(sr);
-            }
-        }
-    }
 }
