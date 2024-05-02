872e0610bb6ae12f185776a50fd2faa4de0abc5f
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-05-02 22:05:29 +0100

    WIP: get backlog test

---
+++ BacklogTracker.Tests/BacklogUpdateTests.cs
@@ -0,0 +1,22 @@
+using BacklogTracker.Data;
+using BacklogTracker.Services;
+using Moq;
+
+namespace BacklogTracker.Tests;
+
+public class BacklogUpdateTests
+{
+    [Fact]
+    public void GetUsersBacklog_SingleUser_ReturnsAllIDsInBacklog()
+    {
+        var mockDbContext = new Mock<ApplicationDbContext>();
+        var expectedUser = new BacklogTrackerUser { Id = "1", Email = "testuser@test.com", GameIDs = new List<string>() { "1234", "5678", "9012" } };
+        mockDbContext.Setup(db => db.Users.FirstOrDefault(u => u.Id == "1")).Returns(expectedUser);
+
+        var backlogService = new BacklogService(mockDbContext.Object);
+
+        var backlog = backlogService.GetBacklog(expectedUser.Email);
+
+        //Assert.Contains(backlog, new List<string>() { "1234", "5678", "9012" });
+    }
+}
