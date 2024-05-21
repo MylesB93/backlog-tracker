ada068e288ac23c048e26dc069ccb28c2c340164
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-05-21 18:34:50 +0100

    Add migration for completed games table

--- BacklogTracker/Data/BacklogTrackerUser.cs
+++ BacklogTracker/Data/BacklogTrackerUser.cs
@@ -5,5 +5,6 @@ namespace BacklogTracker.Data
 	public class BacklogTrackerUser : IdentityUser
 	{
 		public List<string>? GameIDs { get; set; }
+		public List<string>? CompletedGameIDs { get; set; }
 	}
 }
---
+++ BacklogTracker/Data/Migrations/20240521173105_AddCompletedGamescolumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BacklogTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompletedGamescolumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CompletedGameIDs",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompletedGameIDs",
+                table: "AspNetUsers");
+        }
+    }
+}
