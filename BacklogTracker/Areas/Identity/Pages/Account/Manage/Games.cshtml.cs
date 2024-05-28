ebb6e99c51311b47cfd9d6c713d25f4a19634bb6
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-05-28 22:20:34 +0100

    Display completed games on user's games page

--- BacklogTracker/Areas/Identity/Pages/Account/Manage/Games.cshtml.cs
+++ BacklogTracker/Areas/Identity/Pages/Account/Manage/Games.cshtml.cs
@@ -12,25 +12,31 @@ namespace BacklogTracker.Areas.Identity.Pages.Account.Manage
         private readonly IBacklogService _backlogService;
 
         public Response Backlog { get; set; }
+        public Response CompletedGames { get; set; }
 
         public GamesModel(IGameService gameService, IBacklogService backlogService) 
         {
             _gameService = gameService;
             _backlogService = backlogService;
             Backlog = new Response();
+            CompletedGames = new Response();
         }
         public async Task OnGet()
         {
             var email = User?.Identity?.Name;
 
-            // Get list of user's backlog game ID's from backlog service
             var gameIds = email != null ? _backlogService.GetBacklog(email) : new List<string>();
+            var completedGameIds = email != null ? _backlogService.GetCompleted(email) : new List<string>();
             
-            // User that list to get list of BacklogGame from game service
             if (gameIds != null && gameIds.Any())
             {
                 Backlog = await _gameService.GetUsersGamesAsync(gameIds);
-            }            
-        }
+            }
+
+			if (completedGameIds != null && completedGameIds.Any())
+			{
+				CompletedGames = await _gameService.GetUsersGamesAsync(completedGameIds);
+			}
+		}
     }
 }
--- BacklogTracker/wwwroot/js/site.js
+++ BacklogTracker/wwwroot/js/site.js
@@ -16,10 +16,6 @@
         .then(response => {
             if (response.ok)
                 return response.json();
-            else if (response.status === 409)
-                return response.json().then(data => {
-                    throw new Error(data.errorMessage);
-                });
             else
                 throw new Error('Network response was not ok.')
         })
@@ -32,7 +28,7 @@
             console.error('There was a problem sending data to the API:', error);
             errorMsg.innerHTML = error;
             errorMsg.style.visibility = 'visible';
-        });        
+        });
 
 }
 
@@ -42,6 +38,39 @@ function removeFromBacklog(element, email) {
     const requestData = {
         Email: email,
         GameID: element.getAttribute("data-game-id")
+    };
+
+    fetch(apiUrl, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+    })
+        .then(response => {
+            if (response.ok)
+                return response.json();
+            else
+                throw new Error('Network response was not ok.')
+        })
+        .then(data => {
+            console.log('Data successfully sent to the API:', data);
+            location.reload();
+        })
+        .catch(error => {
+            var errorMsg = document.getElementById("errorMessage");
+            console.error('There was a problem sending data to the API:', error);
+            errorMsg.innerHTML = error;
+            errorMsg.style.visibility = 'visible';
+        });
+}
+
+function addToCompleted(element, email) {
+    const apiUrl = '/api/backlog/add-game-to-completed-games';
+
+    const requestData = {
+        Email: email,
+        GameID: element.getAttribute("data-completed-game-id")
     };
 
     fetch(apiUrl, {
