8b493814d9e7abb34d8725deb7f0dba77e2b3e0b
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-06-03 18:50:02 +0100

    Null check game ID's

--- BacklogTracker/Repositories/UserRepository.cs
+++ BacklogTracker/Repositories/UserRepository.cs
@@ -7,11 +7,11 @@ namespace BacklogTracker.Repositories
 	public class UserRepository : IUserRepository
 	{
 		private readonly ApplicationDbContext _dbContext;
-		public UserRepository(ApplicationDbContext dbContext) 
-		{ 
+		public UserRepository(ApplicationDbContext dbContext)
+		{
 			_dbContext = dbContext;
 		}
-		
+
 		public void AddToUsersBacklog(UserDto userDto)
 		{
 			if (string.IsNullOrEmpty(userDto.GameID) || string.IsNullOrEmpty(userDto.Email))
@@ -27,16 +27,19 @@ namespace BacklogTracker.Repositories
 
 			var gameIDs = user.GameIDs;
 
-			if (gameIDs.Contains(userDto.GameID))
+			if (gameIDs != null && gameIDs.Contains(userDto.GameID))
 			{
 				throw new ArgumentException("GameID already exists in the user's backlog.");
 			}
 
-			gameIDs.Add(userDto.GameID);
-			_dbContext.SaveChanges();
+			if (gameIDs != null)
+			{
+				gameIDs.Add(userDto.GameID);
+				_dbContext.SaveChanges();
+			}
 		}
 
-		public void RemoveFromUsersBacklog(UserDto userDto) 
+		public void RemoveFromUsersBacklog(UserDto userDto)
 		{
 			if (string.IsNullOrEmpty(userDto.GameID) || string.IsNullOrEmpty(userDto.Email))
 			{
@@ -97,12 +100,12 @@ namespace BacklogTracker.Repositories
 			var gameIDs = user.GameIDs;
 			var completedGameIDs = user.CompletedGameIDs;
 
-			if (gameIDs.Contains(userDto.GameID) && !completedGameIDs.Contains(userDto.GameID))
+			if (gameIDs != null && completedGameIDs != null && gameIDs.Contains(userDto.GameID) && !completedGameIDs.Contains(userDto.GameID))
 			{
 				completedGameIDs.Add(userDto.GameID);
 				gameIDs.Remove(userDto.GameID);
 				_dbContext.SaveChanges();
-			}			
+			}
 		}
 
 		public List<string>? GetUsersCompletedGames(string email)
