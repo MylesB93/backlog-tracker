5233f03eb6e2e188188bd0ccf7854617f237575c
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-06-10 17:49:26 +0100

    Test for get users backlog

--- BacklogTracker.Tests/BacklogUpdateTests.cs
+++ BacklogTracker.Tests/BacklogUpdateTests.cs
@@ -2,26 +2,33 @@ using BacklogTracker.Data;
 using BacklogTracker.Interfaces;
 using BacklogTracker.Services;
 using Moq;
-using BacklogTracker.Data.DTOs;
-using BacklogTracker.Repositories;
+using BacklogTracker.Controllers.API;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BacklogTracker.Tests;
 
 public class BacklogUpdateTests
 {
+	private readonly Mock<IBacklogService> _backlogServiceMock;
+	private readonly BacklogController _controller;
+
 	// For reference - https://code-maze.com/unit-testing-aspnetcore-web-api/
 	public BacklogUpdateTests()
 	{
-		// TODO: New-up api controller and mock backlog service
+		_backlogServiceMock = new Mock<IBacklogService>();
+		_backlogServiceMock.Setup(s => s.GetBacklog("mylesbroomestest@hotmail.co.uk")).Returns(new List<string>() { "1234", "4567", "7890" });
+
+		_controller = new BacklogController(_backlogServiceMock.Object);
 	}
 
 	[Fact]
 	public void GetUsersBacklog_ReturnsListOfGameIds()
 	{
-		// Arrange
-
 		// Act
+		var result = _controller.GetUsersBacklog("mylesbroomestest@hotmail.co.uk") as OkObjectResult;
 
 		// Assert
+		var gameIds = Assert.IsType<List<string>>(result?.Value);
+		Assert.Equal(3, gameIds.Count);
 	}
 }
