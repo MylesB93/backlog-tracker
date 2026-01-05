407f44c1f74b16a366fc94d7f4962a5c0372c319
Myles Broomes <mylesbroomes@hotmail.co.uk>
2026-01-05 20:51:37 +0000

    WIP: inject dependencies

--- BacklogTracker.Infrastructure/Services/IGDBService.cs
+++ BacklogTracker.Infrastructure/Services/IGDBService.cs
@@ -1,10 +1,24 @@
 ﻿using BacklogTracker.Application.Interfaces;
+using BacklogTracker.Infrastructure.Configuration;
 using BacklogTracker.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace BacklogTracker.Infrastructure.Services
 {
 	public class IGDBService : IGameService
 	{
+		private readonly ILogger<IGDBService> _logger;
+		private IOptions<IGDBConfiguration> _igdbConfiguration;
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public IGDBService(ILogger<IGDBService> logger, IOptions<IGDBConfiguration> igdbConfiguration, IHttpClientFactory httpClientFactory) 
+		{
+			_logger = logger;
+			_httpClientFactory = httpClientFactory;
+			_igdbConfiguration = igdbConfiguration;
+		}
+
 		public Task<Response> GetGamesAsync(string? query)
 		{
 			throw new NotImplementedException();
