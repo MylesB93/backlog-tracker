6babe3f44933d3a1a42677e21173a1231a48866c
Myles Broomes <mylesbroomes@hotmail.co.uk>
2026-01-05 20:44:59 +0000

    WIP: igdb service implementation

---
+++ BacklogTracker.Infrastructure/Services/IGDBService.cs
@@ -0,0 +1,18 @@
+﻿using BacklogTracker.Application.Interfaces;
+using BacklogTracker.Models;
+
+namespace BacklogTracker.Infrastructure.Services
+{
+	public class IGDBService : IGameService
+	{
+		public Task<Response> GetGamesAsync(string? query)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Response> GetUsersGamesAsync(List<string> gameIds)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
