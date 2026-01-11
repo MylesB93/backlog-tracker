ed4c29a6605692ef27ffe742e305a7a8230ae1ee
Myles Broomes <mylesbroomes@hotmail.co.uk>
2026-01-11 21:24:07 +0000

    WIP: add domain layer

---
+++ BacklogTracker.Domain/Entities/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace BacklogTracker.Domain.Entities
+{
+	public class Game
+	{
+		public string? Id { get; set; }
+		public string? Name { get; set; }
+		public string? Url { get; set; }
+		public string? Description { get; set; }
+	}
+}
---
+++ BacklogTracker.Domain/Entities/GameCollection.cs
@@ -0,0 +1,7 @@
+﻿namespace BacklogTracker.Domain.Entities
+{
+	public class GameCollection
+	{
+		public List<Game>? Games { get; set; }
+	}
+}
