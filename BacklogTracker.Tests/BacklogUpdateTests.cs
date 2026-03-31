c59ad97917f63b5ab7128a5c8473162a1dbfcfa9
Myles Broomes <mylesbroomes@hotmail.co.uk>
2026-03-31 19:15:12 +0100

    Additional backlog controller tests

--- BacklogTracker.Tests/BacklogUpdateTests.cs
+++ BacklogTracker.Tests/BacklogUpdateTests.cs
@@ -53,7 +53,14 @@ public class BacklogUpdateTests
 		_backlogServiceMock.Setup(s => s.AddToCompleted(_userDtoWithoutEmail)).Throws(new Exception());
 		_backlogServiceMock.Setup(s => s.AddToCompleted(_userDtoWithoutGameID)).Throws(new Exception());
 		_backlogServiceMock.Setup(s => s.AddToCompleted(_nonExistentUserDto)).Throws(new Exception());
-		//_backlogServiceMock.Setup(s => s.AddToCompleted(_existingGameIDUserDto)).Throws(new ArgumentException());		
+		//_backlogServiceMock.Setup(s => s.AddToCompleted(_existingGameIDUserDto)).Throws(new ArgumentException());
+
+		// Setup RemoveFromCompleted() tests
+		_backlogServiceMock.Setup(s => s.RemoveFromCompleted(_validUserDto));
+		_backlogServiceMock.Setup(s => s.RemoveFromCompleted(_userDtoWithoutEmail)).Throws(new Exception());
+		_backlogServiceMock.Setup(s => s.RemoveFromCompleted(_userDtoWithoutGameID)).Throws(new Exception());
+		_backlogServiceMock.Setup(s => s.RemoveFromCompleted(_nonExistentUserDto)).Throws(new Exception());
+		_backlogServiceMock.Setup(s => s.RemoveFromCompleted(_gameNotInBacklogUserDto)).Throws(new Exception());
 
 		_controller = new BacklogController(_backlogServiceMock.Object);
 	}
@@ -248,5 +255,191 @@ public class BacklogUpdateTests
 
 		// Assert
 		Assert.Equal(500, result?.StatusCode);
+	}
+
+	[Fact]
+	public void MoveFromCompletedToBacklog_WhenUserDtoHasNoGameID_Returns500StatusCode()
+	{
+		// Act
+		var result = _controller.MoveFromCompletedToBacklog(_userDtoWithoutGameID) as ObjectResult;
+
+		// Assert
+		Assert.Equal(500, result?.StatusCode);
+	}
+
+	[Fact]
+	public void MoveFromCompletedToBacklog_WhenUserDoesntExist_Returns500StatusCode()
+	{
+		// Act
+		var result = _controller.MoveFromCompletedToBacklog(_nonExistentUserDto) as ObjectResult;
+
+		// Assert
+		Assert.Equal(500, result?.StatusCode);
+	}
+
+	[Fact]
+	public void MoveFromCompletedToBacklog_WhenGameNotInCompleted_Returns500StatusCode()
+	{
+		// Act
+		var result = _controller.MoveFromCompletedToBacklog(_gameNotInBacklogUserDto) as ObjectResult;
+
+		// Assert
+		Assert.Equal(500, result?.StatusCode);
+	}
+
+	// Response body assertions
+
+	[Fact]
+	public void GetUsersBacklog_WhenCalled_ReturnsCorrectGameIds()
+	{
+		// Act
+		var result = _controller.GetUsersBacklog("mylesbroomestest@hotmail.co.uk") as OkObjectResult;
+
+		// Assert
+		var gameIds = Assert.IsType<List<string>>(result?.Value);
+		Assert.Contains("1234", gameIds);
+		Assert.Contains("4567", gameIds);
+		Assert.Contains("7890", gameIds);
+	}
+
+	[Fact]
+	public void AddToBacklog_WhenCalled_ReturnsExpectedMessage()
+	{
+		// Act
+		var result = _controller.AddToBacklog(_validUserDto) as OkObjectResult;
+
+		// Assert
+		Assert.NotNull(result?.Value);
+		var message = result.Value.GetType().GetProperty("Message")?.GetValue(result.Value)?.ToString();
+		Assert.Equal("Data saved successfully.", message);
+	}
+
+	[Fact]
+	public void RemoveFromBacklog_WhenCalled_ReturnsExpectedMessage()
+	{
+		// Act
+		var result = _controller.RemoveFromBacklog(_validUserDto) as OkObjectResult;
+
+		// Assert
+		Assert.NotNull(result?.Value);
+		var message = result.Value.GetType().GetProperty("Message")?.GetValue(result.Value)?.ToString();
+		Assert.Equal("Data saved successfully.", message);
+	}
+
+	[Fact]
+	public void AddToCompleted_WhenCalled_ReturnsExpectedMessage()
+	{
+		// Act
+		var result = _controller.AddToCompleted(_validUserDto) as OkObjectResult;
+
+		// Assert
+		Assert.NotNull(result?.Value);
+		var message = result.Value.GetType().GetProperty("Message")?.GetValue(result.Value)?.ToString();
+		Assert.Equal("Data saved successfully.", message);
+	}
+
+	[Fact]
+	public void MoveFromCompletedToBacklog_WhenCalled_ReturnsExpectedMessage()
+	{
+		// Act
+		var result = _controller.MoveFromCompletedToBacklog(_validUserDto) as OkObjectResult;
+
+		// Assert
+		Assert.NotNull(result?.Value);
+		var message = result.Value.GetType().GetProperty("Message")?.GetValue(result.Value)?.ToString();
+		Assert.Equal("Data saved successfully.", message);
+	}
+
+	// Verify service method invocations
+
+	[Fact]
+	public void AddToBacklog_WhenCalled_InvokesServiceExactlyOnce()
+	{
+		// Act
+		_controller.AddToBacklog(_validUserDto);
+
+		// Assert
+		_backlogServiceMock.Verify(s => s.AddToBacklog(_validUserDto), Times.Once);
+	}
+
+	[Fact]
+	public void RemoveFromBacklog_WhenCalled_InvokesServiceExactlyOnce()
+	{
+		// Act
+		_controller.RemoveFromBacklog(_validUserDto);
+
+		// Assert
+		_backlogServiceMock.Verify(s => s.RemoveFromBacklog(_validUserDto), Times.Once);
+	}
+
+	[Fact]
+	public void AddToCompleted_WhenCalled_InvokesServiceExactlyOnce()
+	{
+		// Act
+		_controller.AddToCompleted(_validUserDto);
+
+		// Assert
+		_backlogServiceMock.Verify(s => s.AddToCompleted(_validUserDto), Times.Once);
+	}
+
+	[Fact]
+	public void MoveFromCompletedToBacklog_WhenCalled_InvokesRemoveFromCompletedAndAddToBacklog()
+	{
+		// Act
+		_controller.MoveFromCompletedToBacklog(_validUserDto);
+
+		// Assert
+		_backlogServiceMock.Verify(s => s.RemoveFromCompleted(_validUserDto), Times.Once);
+		_backlogServiceMock.Verify(s => s.AddToBacklog(_validUserDto), Times.Once);
+	}
+
+	[Fact]
+	public void GetUsersBacklog_WhenCalled_InvokesServiceExactlyOnce()
+	{
+		// Act
+		_controller.GetUsersBacklog("mylesbroomestest@hotmail.co.uk");
+
+		// Assert
+		_backlogServiceMock.Verify(s => s.GetBacklog("mylesbroomestest@hotmail.co.uk"), Times.Once);
+	}
+
+	// Error message assertions
+
+	[Fact]
+	public void GetUsersBacklog_WhenUserDoesntExist_ReturnsErrorMessageInBody()
+	{
+		// Act
+		var result = _controller.GetUsersBacklog("invalidemail@test.com") as ObjectResult;
+
+		// Assert
+		Assert.NotNull(result?.Value);
+		Assert.Contains("An error occurred", result.Value.ToString());
+	}
+
+	[Fact]
+	public void AddToBacklog_WhenGameIDAlreadyExists_ReturnsConflictMessage()
+	{
+		// Act
+		var result = _controller.AddToBacklog(_existingGameIDUserDto) as ObjectResult;
+
+		// Assert
+		Assert.Equal(409, result?.StatusCode);
+		Assert.NotNull(result?.Value);
+	}
+
+	// GetBacklog returns null scenario
+
+	[Fact]
+	public void GetUsersBacklog_WhenServiceReturnsNull_ReturnsEmptyList()
+	{
+		// Arrange
+		_backlogServiceMock.Setup(s => s.GetBacklog("nullbacklog@test.com")).Returns((List<string>?)null);
+
+		// Act
+		var result = _controller.GetUsersBacklog("nullbacklog@test.com") as OkObjectResult;
+
+		// Assert
+		var gameIds = Assert.IsType<List<string>>(result?.Value);
+		Assert.Empty(gameIds);
 	}
 }
